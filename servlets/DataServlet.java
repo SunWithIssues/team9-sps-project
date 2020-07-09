7164260b6473472d843db9e2bcef3fd2dab7f5c5
AE <anya.a.ellis@gmail.com>
2020-07-09 03:58:42 -0400

    Code should display a list. This might not run.

--- servlets/DataServlet.java
+++ servlets/DataServlet.java
@@ -1,3 +1,12 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.Gson;
 
 /**
  *  Creates a page of json (or gson) that stores
@@ -7,6 +16,26 @@
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-    
+  @Override
+  public void init() {
+    phrases = new ArrayList<>();
+    phrases.add("1. Empty spot for link or phrase");
+    phrases.add("2. Empty spot for link or phrase");
+    phrases.add("3. Empty spot for link or phrase");
+  }  
+
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String json = convertToJson(phrases);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+  private String convertToJson(ArrayList sample) {
+    Gson gson = new Gson();
+    String json = gson.toJson(sample);
+    return json;
+  }
 }
  
