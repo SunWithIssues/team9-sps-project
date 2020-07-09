966317077284c514e713bc79976ae2558cafc744
AE <anya.a.ellis@gmail.com>
2020-07-09 03:19:42 -0400

    Class stub

---
+++ DataServlet.java
@@ -0,0 +1,8 @@
+
+/**
+ *  Creates a page of json (or gson) that stores
+ *    links or phrases.
+ *  TODO: Have it use a google datastore.
+ */
+@WebServlet("/data")
+public class DataServlet extends HttpServlet {}
