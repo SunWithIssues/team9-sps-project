5b66e668343a4af3d04e2294b5d49bae76632ec6
Katherine Sirois <katherine.sirois.2@gmail.com>
2020-07-19 15:29:44 -0400

    More tests 11

--- SustainaBrowse/src/main/extension/background.js
+++ SustainaBrowse/src/main/extension/background.js
@@ -1,19 +1,22 @@
-chrome.browserAction.onClicked.addListener(
-    function (activeTab) {
-        console.log("testing")
-        const tips = [
-            'You miss 100% of the shots you don\'t take', 
-            'Sometimes it\'s the very people no one imagines anything of that do the things no one can imagine', 
-            'You have to be odd to be number one', 'Why fit in when you were born to stand out'
-        ];
+chrome.browserAction.onClicked.addListener(test());
+    
+function test(){
+    console.log("testing");
+}
+    
+function getRandomTip() {
+    const tips = [
+        'You miss 100% of the shots you don\'t take', 
+        'Sometimes it\'s the very people no one imagines anything of that do the things no one can imagine', 
+        'You have to be odd to be number one', 'Why fit in when you were born to stand out'
+    ];
 
-        // Pick a random greeting.
-        const tip = tips[Math.floor(Math.random() * tips.length)];
-        console.log(tip);
+    // Pick a random greeting.
+    const tip = tips[Math.floor(Math.random() * tips.length)];
 
-        // Add it to the page.
-        const tipContainer = document.getElementById('tip-container');
-        tipContainer.innerText = tip;
-    });
+    // Add it to the page.
+    const tipContainer = document.getElementById('tip-container');
+    tipContainer.innerText = tip;
+}
   
   
