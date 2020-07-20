560572a3d69748b8034ad43518cad93232aa97ef
Katherine Sirois <katherine.sirois.2@gmail.com>
2020-07-19 21:10:16 -0400

    More tests 18

--- SustainaBrowse/src/main/extension/popup.js
+++ SustainaBrowse/src/main/extension/popup.js
@@ -1,5 +1,16 @@
-document.getElementById("aboutButton").addEventListener("click", visitPage);
+document.getElementById("tipButton").addEventListener("click", visitPage);
 
-function visitPage(){
-    window.location='http://www.google.com';
+function getRandomTip() {
+    const tips = [
+        'You miss 100% of the shots you don\'t take', 
+        'Sometimes it\'s the very people no one imagines anything of that do the things no one can imagine', 
+        'You have to be odd to be number one', 'Why fit in when you were born to stand out'
+    ];
+
+    // Pick a random greeting.
+    const tip = tips[Math.floor(Math.random() * tips.length)];
+    console.log(tip);
+    // Add it to the page.
+    const tipContainer = document.getElementById('tip-container');
+    tipContainer.innerText = tip;
 }
